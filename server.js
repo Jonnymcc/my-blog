c53b94cf0c186aa12e225d9bf6643cb057384e44
Jonathan McCall <jon.mccall@newscred.com>
2016-02-01 22:11:36 -0500

    Listen on port 80 instead of 3000

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('/about', function (req, res) {
     res.render('about');
 });
 
-app.listen(3000, function () {
-    console.log('Blog listening on port 3000!');
+app.listen(80, function () {
+    console.log('Blog listening on port 80!');
 });
