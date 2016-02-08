43e82957fee5bfc9f24b589723519ebf739f3012
Jonathan McCall <jon.mccall@newscred.com>
2016-02-07 21:33:33 -0500

    Extract routes to a separate module

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var app = express();
+var routes = require('./routes');
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
 app.use(express.static('static'));
 
-
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
-app.get('/about', function (req, res) {
-    res.render('about');
-});
+app.use('/', routes);
 
 app.listen(80, function () {
     console.log('Blog listening on port 80!');
