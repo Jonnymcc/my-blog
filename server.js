9b4f4a6f1d846be93f2ef455bbe00d376bd722d2
Jonathan McCall <jon.mccall@newscred.com>
2016-02-07 22:26:25 -0500

    Add access log

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var routes = require('./routes');
+var morgan = require('morgan')
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
 app.use(express.static('static'));
+
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+app.use(morgan('short', {stream: accessLogStream}))
 
 app.use('/', routes);
 
